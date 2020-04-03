3baecb0621141a8268c372f0456f492b63360d8c
Daniel R. Neal <dan.r.neal@gmail.com>
2020-04-02 17:18:59 -0700

    Initial commit

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,14 @@
+/* @TODO replace with your variables
+ * ensure all variables on this page match your project
+ */
+
+export const environment = {
+  production: false,
+  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+  auth0: {
+    url: 'full-stack-cafe.auth0.com', // the auth0 domain prefix
+    audience: 'http://127.0.0.1/', // the audience set for the auth0 app
+    clientId: 'SC8aft2Ugm7C1aF3ZEJATWFkBf4NpXW2', // the client id generated for the auth0 app
+    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+  }
+};
