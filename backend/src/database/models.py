49083388d151f12315becb2f77e642fbafea95b7
Daniel R. Neal <dan.r.neal@gmail.com>
2020-04-03 13:24:17 -0700

    refactor: Split db_path into component globals

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -3,9 +3,10 @@ from sqlalchemy import Column, String, Integer
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_filename = "database.db"
-project_dir = os.path.dirname(os.path.abspath(__file__))
-database_path = "sqlite:///{}".format(os.path.join(project_dir, database_filename))
+DB_DIALECT = 'sqlite'
+DB_NAME = 'database'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = f'sqlite:///{os.path.join(PROJECT_DIR, DB_NAME)}'
 
 db = SQLAlchemy()
 
