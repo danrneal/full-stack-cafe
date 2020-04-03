c9a3cea4108d597461848f7adf69dc882e937089
Daniel R. Neal <dan.r.neal@gmail.com>
2020-04-03 16:34:10 -0700

    feat: Implement AuthError exception for authorization errors

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -7,14 +7,17 @@ from urllib.request import urlopen
 
 AUTH0_DOMAIN = 'full-stack-cafe.auth0.com'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = 'dev'
+API_AUDIENCE = 'http://127.0.0.1/'
 
-## AuthError Exception
-'''
-AuthError Exception
-A standardized way to communicate auth failure modes
-'''
+
 class AuthError(Exception):
+    """Creates an exception to handle authorization errors
+
+    Attributes:
+        error: A dict containing information about the error
+        status_code: An int representing the http status code
+    """
+
     def __init__(self, error, status_code):
         self.error = error
         self.status_code = status_code
