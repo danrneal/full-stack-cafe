0e23bc924aa222f1eb8ab8cbafdf6780549361c1
Daniel R. Neal <dan.r.neal@gmail.com>
2020-04-04 10:43:21 -0700

    style: Remove unused imports

--- backend/src/api.py
+++ backend/src/api.py
@@ -1,11 +1,6 @@
-import os
-from flask import Flask, request, jsonify, abort
-from sqlalchemy import exc
-import json
+from flask import Flask, jsonify
 from flask_cors import CORS
-
-from .database.models import db_drop_and_create_all, setup_db, Drink
-from .auth.auth import AuthError, requires_auth
+from .database.models import setup_db, Drink
 
 app = Flask(__name__)
 setup_db(app)
