c9e40522c46b235fb11c7e59316ca05aab46353e
Daniel R. Neal <dan.r.neal@gmail.com>
2020-04-04 16:36:24 -0700

    feat: Implement ability to delete a drink

--- backend/src/api.py
+++ backend/src/api.py
@@ -163,17 +163,31 @@ def patch_book_rating(drink_id):
     return response
 
 
-'''
-@TODO implement endpoint
-    DELETE /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should delete the corresponding row for <id>
-        it should require the 'delete:drinks' permission
-    returns status code 200 and json {"success": True, "delete": id} where id
-        is the id of the deleted record or appropriate status code indicating
-        reason for failure
-'''
+@requires_auth('delete:drinks')
+@app.route('/drinks/<int:drink_id>', methods=['DELETE'])
+def delete_drink(drink_id):
+    """Route handler for endpoint to delete a single drink
+
+    Args:
+        drink_id: An int representing the identifier for a drink to delete
+
+    Returns:
+        response: A json object containing the id of the drink that was deleted
+    """
+
+    drink = Drink.query.get(drink_id)
+
+    if drink is None:
+        abort(422)
+
+    drink.delete()
+
+    response = jsonify({
+        'success': True,
+        'deleted_drink_id': drink_id,
+    })
+
+    return response
 
 
 @app.errorhandler(400)
