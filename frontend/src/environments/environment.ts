db9156be097b0aff186085e1c48a0fb802ae4896
Daniel R. Neal <dan.r.neal@gmail.com>
2020-04-06 10:11:25 -0700

    fix: Include correct env vars for frontend

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'full-stack-cafe.auth0.com', // the auth0 domain prefix
+    url: 'full-stack-cafe', // the auth0 domain prefix
     audience: 'http://127.0.0.1/', // the audience set for the auth0 app
     clientId: 'SC8aft2Ugm7C1aF3ZEJATWFkBf4NpXW2', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    callbackURL: 'http://127.0.0.1:8100', // the base url of the running ionic application. 
   }
 };
