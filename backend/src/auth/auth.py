d322221bd81b349d34bab0d6122828d671809eb1
Daniel R. Neal <dan.r.neal@gmail.com>
2020-04-06 10:11:56 -0700

    fix: No need to return payload from auth decorator

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -212,7 +212,7 @@ def requires_auth(permission=''):
             token = get_token_auth_header()
             payload = verify_decode_jwt(token)
             check_permissions(permission, payload)
-            return f(payload, *args, **kwargs)
+            return f(*args, **kwargs)
 
         return wrapper
 
